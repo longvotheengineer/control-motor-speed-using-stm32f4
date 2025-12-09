b38f9b41d246c97b969ca647d13e557a14c8104b
lvtheenginneer <long.vo060204@hcmut.edu.vn>
2025-12-09 10:21:28 +0700

    Add UARTConnection_t structure to UART.c
    Added UARTConnection_t structure for UART connection management.

--- MDK-ARM/UART.c
+++ MDK-ARM/UART.c
@@ -1,5 +1,7 @@
 #include "uart.h"
 
+UARTConnection_t uart_connection;
+
 uint8_t 	uart_tx_buffer 		[UART_TX_BUFFER_SIZE];
 uint8_t 	uart_rx_buffer		[UART_RX_BUFFER_SIZE];
 uint8_t 	uart_rx_cmd		[UART_CMD_SIZE];	
